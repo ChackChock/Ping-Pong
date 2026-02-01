1665b8921e7a62843845917b4249f74efaf76dc7
Burkin Nikita <92577296+ChackChock@users.noreply.github.com>
2026-02-01 10:06:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from turtle import right
+from random import random
 import pygame
 
 
@@ -27,6 +27,54 @@ class Player(Sprite):
             else:
                 self.rect.y += self.speed
 
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > 600:
+            self.rect.bottom = 600
+
+
+class Ball(Sprite):
+    def __init__(self, center, image, speed):
+        super().__init__(center, image)
+        self.speed = speed
+        self.velocity = pygame.Vector2(1, 0)
+
+    def check_x_collision(self, player):
+        if self.rect.colliderect(player.rect):
+            if self.velocity.x > 0:
+                self.rect.right = player.rect.left
+            else:
+                self.rect.left = player.rect.right
+            self.velocity.x = -self.velocity.x
+            self.velocity.rotate_ip((random() - 0.5) * 15)
+            self.speed += 0.5
+
+    def check_y_collision(self, player):
+        if self.rect.colliderect(player.rect):
+            if self.velocity.y > 0:
+                self.rect.bottom = player.rect.top
+            else:
+                self.rect.top = player.rect.bottom
+            self.velocity.y = -self.velocity.y
+
+    def update(self, left_player, right_player):
+        vector = self.velocity * self.speed
+
+        self.rect.x += vector.x
+        self.check_x_collision(left_player)
+        self.check_x_collision(right_player)
+
+        self.rect.y += vector.y
+        self.check_y_collision(left_player)
+        self.check_y_collision(right_player)
+
+        if self.rect.top <= 0:
+            self.rect.top = 0
+            self.velocity.y = -self.velocity.y
+        if self.rect.bottom >= 600:
+            self.rect.bottom = 600
+            self.velocity.y = -self.velocity.y
+
 
 window = pygame.Window("Ping Pong", (800, 600), pygame.WINDOWPOS_CENTERED)
 surface = window.get_surface()  # surface - поверхность
@@ -36,6 +84,11 @@ image = pygame.Surface((40, 100))
 image.fill("orange")
 left_player = Player((40, 300), image, 10)
 right_player = Player((760, 300), image, 10)
+
+image = pygame.Surface((30, 30))
+image.fill("white")
+pygame.draw.aacircle(image, "red", (15, 15), 15)
+ball = Ball((400, 300), image, 5)
 
 running = True
 while running:
@@ -74,12 +127,20 @@ while running:
 
     # Обновление объектов
     left_player.update()
+    right_player.update()
+    ball.update(left_player, right_player)
+
+    # if ball.rect.colliderect(left_player.rect) or ball.rect.colliderect(
+    #     right_player.rect
+    # ):
+    #     ball.velocity.x = -ball.velocity.x
 
     # Отрисовка
     surface.fill("white")
 
     left_player.render(surface)
     right_player.render(surface)
+    ball.render(surface)
 
     window.flip()
     clock.tick(60)
